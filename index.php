e029267603e897b8fb676ccebbc007dd1ffe0299
Nwachukwu Ossai <chuks@Nwachukwus-MacBook-Pro.local>
2021-06-29 05:46:29 +0300

    doing v0.0.1 ...just text

--- index.php
+++ index.php
@@ -1,1 +1,5 @@
-<?php include_once("home.html"); ?>
+<?php 
+# https://www.geeksforgeeks.org/how-to-deploy-a-basic-static-html-website-to-heroku/
+# http://aliensix.com/particles-1.4/HTML/ideology-static.html
+include_once("home.html"); 
+?>
